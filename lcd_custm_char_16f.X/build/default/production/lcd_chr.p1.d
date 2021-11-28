35200ff9af9674db7d3c02aeacd45987051290ff
Bala <bala@zmail.com>
2021-11-28 14:04:35 +0530

    project files added

---
+++ lcd_custm_char_16f.X/build/default/production/lcd_chr.p1.d
@@ -0,0 +1,2 @@
+build/default/production/lcd_chr.p1:  \
+lcd_chr.c 
---
+++ lcd_custm_char_16f.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=lcd_custm_char_16f.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/lcd_custm_char_16f.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=lcdcustmchar16f.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/lcdcustmchar16f.x.tar
